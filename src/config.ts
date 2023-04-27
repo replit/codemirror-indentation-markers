replit/codemirror-indentation-markers

commit 581f46dc30d21e3bf035ea60b687c5f956d4ca26

Fix circular dependency issue by moving config related code to a new file (#14)

src/config.ts
@@ -1,0 +1,20 @@
+export interface IndentationMarkerConfiguration {
+    /**
+     * Determines whether active block marker is styled differently.
+     */
+    highlightActiveBlock?: boolean
+
+    /**
+     * Determines whether markers in the first column are omitted.
+     */
+    hideFirstIndent?: boolean
+}
+
+export const indentationMarkerConfig = Facet.define<IndentationMarkerConfiguration, Required<IndentationMarkerConfiguration>>({
+    combine(configs) {
+        return combineConfig(configs, {
+            highlightActiveBlock: true,
+            hideFirstIndent: false,
+        });
+    }
+});

src/index.ts
@@ -10,6 +10,7 @@ import {
 } from '@codemirror/view';
 import { getCurrentLine, getVisibleLines } from './utils';
 import { IndentEntry, IndentationMap } from './map';
+import { IndentationMarkerConfiguration, indentationMarkerConfig } from "./config";
 
 // CSS classes:
 // - .cm-indent-markers
@@ -97,27 +98,6 @@ function makeBackgroundCSS(entry: IndentEntry, indentWidth: number, hideFirstInd
   return backgrounds.join(',');
 }
 
-interface IndentationMarkerConfiguration {
-  /**
-   * Determines whether active block marker is styled differently.
-   */
-  highlightActiveBlock?: boolean
-
-  /**
-   * Determines whether markers in the first column are omitted.
-   */
-  hideFirstIndent?: boolean
-}
-
-export const indentationMarkerConfig = Facet.define<IndentationMarkerConfiguration, Required<IndentationMarkerConfiguration>>({
-  combine(configs) {
-    return combineConfig(configs, {
-      highlightActiveBlock: true,
-      hideFirstIndent: false,
-    });
-  }
-});
-
 class IndentMarkersClass implements PluginValue {
   view: EditorView;
   decorations!: DecorationSet;
@@ -143,10 +123,10 @@ class IndentMarkersClass implements PluginValue {
     this.currentLineNumber = lineNumber;
     const activeBlockUpdateRequired = update.state.facet(indentationMarkerConfig).highlightActiveBlock && lineNumberChanged;
     if (
-        update.docChanged ||
-        update.viewportChanged ||
-        unitWidthChanged ||
-        activeBlockUpdateRequired
+      update.docChanged ||
+      update.viewportChanged ||
+      unitWidthChanged ||
+      activeBlockUpdateRequired
     ) {
       this.generate(update.state);
     }

src/map.ts
@@ -1,6 +1,6 @@
 import { EditorState, Line } from '@codemirror/state';
 import { getCurrentLine, numColumns } from './utils';
-import {indentationMarkerConfig} from "./index";
+import {indentationMarkerConfig} from "./config";
 
 export interface IndentEntry {
   line: Line;

commit ee117d06ad7330effd3a1f8d22d54474f3a9c358

Build fix (Broken in #14)

src/config.ts
@@ -1,3 +1,5 @@
+import { combineConfig, Facet } from '@codemirror/state';
+
 export interface IndentationMarkerConfiguration {
     /**
      * Determines whether active block marker is styled differently.